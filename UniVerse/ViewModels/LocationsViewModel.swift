4de4b2bd7cf7e8a96ac0c13885eb154f58c15b87
cndiritu <cndiritu@skidmore.edu>
2023-11-14 11:19:41 -0500

    Initial Commit

---
+++ UniVerse/ViewModels/LocationsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LocationsViewModel.swift
+//  UniVerse
+//
+//  Created by Christian  on 11/15/23.
+//
+
+import Foundation
---
+++ UniVerse/Views/LocationView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationView.swift
+//  UniVerse
+//
+//  Created by Christian  on 11/15/23.
+//
+
+import SwiftUI
+
+struct LocationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationView()
+}
