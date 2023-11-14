f1b624ae0933361180718cb4e340ffac646cde8f
cndiritu <cndiritu@skidmore.edu>
2023-11-14 11:19:41 -0500

    Initial Commit

---
+++ UniVerse/Models/Location.swift
@@ -0,0 +1,20 @@
+//
+//  Location.swift
+//  UniVerse
+//
+//  Created by Christian  on 11/12/23.
+//
+
+import Foundation
+import MapKit
+
+
+struct Location{
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
+
---
+++ UniVerse/UniVerseApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniVerseApp.swift
+//  UniVerse
+//
+//  Created by Christian  on 11/12/23.
+//
+
+import SwiftUI
+
+@main
+struct UniVerseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
